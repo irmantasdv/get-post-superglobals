6b77903fdb3ce4c57a322212e2541b2c7df1085a
bhava <irmantas.dv@gmail.com>
2022-09-17 20:23:28 +0300

    initial commit

---
+++ calculator/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pamoka 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form action="calculator.php" method="get">
+        <input type="number" name="num1">
+        <input type="number" name="num2">
+        <button type="submit" name="action" value="sudek">+</button>
+        <button type="submit" name="action" value="atimk">-</button>
+    </form>
+    
+</body>
+</html>
